96cf1cc6389142b0e4b3dcb150df69916a0248cc
amy <amy58328@gmail.com>
2020-03-29 21:59:08 +0800

    Una 3./29

--- 歐歐/oo.cpp
+++ 歐歐/oo.cpp
@@ -4,49 +4,15 @@ using namespace std;
 
 int main()
 {
-	int times;
-	cin >> times;
-	getchar();
-	int len[26],max=0;
-	for(int i=0;i<26;i++)
-	{
-		len[i] = 0;
-	}
-	while(times--)
-	{
-		string line;
-		getline(cin,line);
-		// cout << line << endl;
-		for(int i=0;i<line.length();i++)
-		{
-			if(line[i] > 64 && line[i] < 91)
-			{
-				len[line[i] - 65]++;
-			}
-			else if(line[i] > 96 && line[i] < 123)
-			{
-				len[line[i] - 97]++;
-			}
-			else continue;
-			int tmp = len[i];
-			if(tmp > max)
-			{
-				max = tmp;
-			}
-		}
+	string s = "exp(x) - 3 * x * cos(2*x) - 8.3";
+	int a = -10,b = 2;
+	float e = 0.00000001;
 
+
+	while(abs(a-b) < 2*e)
+	{
+		
 	}
 
-	while(max > 0)
-	{
-		for(int i=0;i<26;i++)
-		{
-			if(len[i] == max)
-			{
-				cout << char(i+65) << " " << len[i] << endl;
-			}
-		}
-		max--;
-	}
 		
 }
